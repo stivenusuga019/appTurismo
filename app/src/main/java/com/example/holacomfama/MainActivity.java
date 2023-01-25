656c5dc8e48aa52bee3e4dfd3df34399c1703b37
emanuelo09 <emanuelou67@gmail.com>
2023-01-24 20:41:52 -0500

    Se crea menu de opciones

--- app/src/main/java/com/example/holacomfama/MainActivity.java
+++ app/src/main/java/com/example/holacomfama/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.holacomfama;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //ATRIBUTOS
 
+    //METODOS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Cargar el menu xml creado previamente
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    //DAR funcionamiento al menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //observamos que item del menu le damos click
+
+        int itemSeleccionado=item.getItemId();
+        switch (itemSeleccionado){
+
+            case(R.id.opcion1):
+                Toast.makeText(this, "Selecionaste Acerca De", Toast.LENGTH_SHORT).show();
+                break;
+            case(R.id.opcion2):
+                Toast.makeText(this, "Selecionaste Ingles", Toast.LENGTH_SHORT).show();
+                break;
+            case(R.id.opcion3):
+                Toast.makeText(this, "Selecionaste Español", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
