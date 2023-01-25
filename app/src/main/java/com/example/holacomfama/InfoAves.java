b2da87799ffd4dc506a0c7a073682d2cdde844ec
stiven muñoz usuga <stivenusuga019@gmail.com>
2023-01-25 14:35:04 -0500

    se crea nuevo activity

---
+++ app/src/main/java/com/example/holacomfama/InfoAves.java
@@ -0,0 +1,14 @@
+package com.example.holacomfama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoAves extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_aves);
+    }
+}
