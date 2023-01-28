bd5a0dab74e2ef6c3e12a4e0d8d9e972e0356f06
emanuelo09 <emanuelou67@gmail.com>
2023-01-27 19:56:00 -0500

    Se cambia el de los activitys

--- app/src/main/java/com/example/holacomfama/InfoAves.java
+++ app/src/main/java/com/example/holacomfama/InfoAves.java
@@ -1,11 +1,18 @@
 package com.example.holacomfama;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class InfoAves extends AppCompatActivity {
 
@@ -13,11 +20,31 @@ public class InfoAves extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_aves);
+        ActionBar barra=getSupportActionBar();
+
+        //Cambiando el color de la barra
+        ColorDrawable colorBarra= new ColorDrawable(Color.parseColor("#D81B41"));
+        barra.setBackgroundDrawable(colorBarra);
     }
     //Cargar el menu xml creado previamente
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
+    }
+    public void cambiarIdioma(String idioma){
+
+        //Configurando el lenguaje del celular
+        Locale lenguaje= new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configuración global en el celular
+
+        Configuration configurationTelefono=getResources().getConfiguration();
+        configurationTelefono.locale=lenguaje;
+
+        //ejecutamos la configuración
+        getBaseContext().getResources().updateConfiguration(configurationTelefono,getBaseContext().getResources().getDisplayMetrics());
+
     }
 
     //DAR funcionamiento al menu
@@ -32,10 +59,14 @@ public class InfoAves extends AppCompatActivity {
                 Toast.makeText(this, "Selecionaste Acerca De", Toast.LENGTH_SHORT).show();
                 break;
             case(R.id.opcion2):
-                Toast.makeText(this, "Selecionaste Ingles", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("en");
+                Intent intent1 = new Intent(InfoAves.this,InfoAves.class);
+                startActivity(intent1);
                 break;
             case(R.id.opcion3):
-                Toast.makeText(this, "Selecionaste Español", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("es");
+                Intent intent2 = new Intent(InfoAves.this,InfoAves.class);
+                startActivity(intent2);
                 break;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/holacomfama/MainActivity.java
+++ app/src/main/java/com/example/holacomfama/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.holacomfama;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     //ATRIBUTOS
@@ -18,7 +24,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar barra=getSupportActionBar();
+
+        //Cambiando el color de la barra
+        ColorDrawable colorBarra= new ColorDrawable(Color.parseColor("#D81B41"));
+        barra.setBackgroundDrawable(colorBarra);
     }
+
+    //Metodo para cambiar el idioma de mi app
+     public void cambiarIdioma(String idioma){
+
+        //Configurando el lenguaje del celular
+         Locale lenguaje= new Locale(idioma);
+         Locale.setDefault(lenguaje);
+
+         //configuración global en el celular
+
+         Configuration configurationTelefono=getResources().getConfiguration();
+         configurationTelefono.locale=lenguaje;
+
+         //ejecutamos la configuración
+         getBaseContext().getResources().updateConfiguration(configurationTelefono,getBaseContext().getResources().getDisplayMetrics());
+
+     }
 
     public void  pajaros(View view){
         Intent pajaros = new Intent(MainActivity.this,InfoAves.class);
@@ -40,13 +69,18 @@ public class MainActivity extends AppCompatActivity {
         switch (itemSeleccionado){
 
             case(R.id.opcion1):
-                Toast.makeText(this, "Selecionaste Acerca De", Toast.LENGTH_SHORT).show();
+                Intent IntentA = new Intent(getApplicationContext(),Acercade.class);
+                startActivity(IntentA);
                 break;
             case(R.id.opcion2):
-                Toast.makeText(this, "Selecionaste Ingles", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("en");
+                Intent intent1 = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent1);
                 break;
             case(R.id.opcion3):
-                Toast.makeText(this, "Selecionaste Español", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("es");
+                Intent intent2 = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent2);
                 break;
         }
         return super.onOptionsItemSelected(item);
